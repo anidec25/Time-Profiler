ade411e33a8aaf91c6375b22904138e11d327cc1
Aniruddha Mukherjee <maniruddha25@gmail.com>
2023-06-16 22:32:57 +0530

    Chart display added

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 import pandas as pd
 import numpy as np
+
 import streamlit as st
+
 from io import StringIO
+
+import altair as alt
+import plotly as plt
 
 
 
@@ -14,15 +19,31 @@ def load_csv():
     return df_input
 
 
+def prep_data(df):
+
+    df_input = df.rename({date_col:"ds",metric_col:"y"},errors='raise',axis=1)
+    st.markdown("The selected date column is now labeled as **ds** and the values columns as **y**")
+    df_input = df_input[['ds','y']]
+    df_input =  df_input.sort_values(by='ds',ascending=True)
+    return df_input
+
+
+st.title('Forecasting Application')
+st.write('This app enables you to generate time series forecast withouth any dependencies.')
+st.markdown("""The forecasting library used is **[Prophet](https://facebook.github.io/prophet/)**.""")
+
+df =  pd.DataFrame()  
+
+
 input = st.file_uploader('')
-    
+
 if input is None:
     st.write("Or use sample dataset to try the application")
     sample = st.checkbox("Download sample data from GitHub")
 
 try:
     if sample:
-        st.markdown("""[download_link](https://gist.github.com/giandata/e0b5c2d2e71d4fd4388295eb5b71aeeb)""")    
+        st.markdown("""[download_link](https://raw.githubusercontent.com/anidec25/Time-Profiler/main/data/daily-minimum-temperatures-in-me.csv)""")    
         
 except:
 
@@ -31,5 +52,37 @@ except:
             df = load_csv()
     
             st.write("Columns:")
-            st.write(list(df.columns))
+            st.write(df.describe())
+
+            st.write(df.head())
             columns = list(df.columns)
+
+            col1,col2 = st.columns(2)
+            with col1:
+                date_col = st.selectbox("Select date column",index= 0,options=columns,key="date")
+            with col2:
+                metric_col = st.selectbox("Select values column",index=1,options=columns,key="values")
+
+            df = prep_data(df)
+            output = 0
+
+
+    if st.checkbox('Chart data',key='show'):
+        with st.spinner('Plotting data..'):
+            col1,col2 = st.columns(2)
+            with col1:
+                st.dataframe(df)
+                
+            with col2:    
+                st.write("Dataframe description:")
+                st.write(df.describe())
+        
+        try:
+                line_chart = alt.Chart(df).mark_line().encode(
+                    x = 'ds:T',
+                    y = "y:Q",tooltip=['ds:T', 'y']).properties(title="Time series preview").interactive()
+                st.altair_chart(line_chart,use_container_width=True)
+                
+        except:
+            st.line_chart(df['y'],use_container_width =True,height = 300)
+
